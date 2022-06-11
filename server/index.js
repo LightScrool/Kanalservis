6ebef9ab689272f34cb8ee61d9105e711feeba49
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-06-12 00:32:48 +0500

    fixed bug with axios fetching by using cors on server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const itemsRouter = require('./routes');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express()
-
+app.use(cors());
 app.use('/api', itemsRouter);
 
 app.listen(PORT, () => console.log('server is working'));
