d68a75e75d79aea35a5517b1693f8a66e5fc4ce5
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-06-11 23:14:47 +0500

    database filling

--- server/database.sql
+++ server/database.sql
@@ -6,8 +6,10 @@ CREATE TABLE items(
     date DATE
 );
 
-INSERT INTO items (title, quantity, distance, "date") VALUES ('Title', 120, 5621, '11.06.2022');
+INSERT INTO items (title, quantity, distance, "date") VALUES
+('Title', 120, 5621, '11.06.2022')
 
 SELECT * FROM items;
 
 SELECT COUNT(*) FROM items;
+
