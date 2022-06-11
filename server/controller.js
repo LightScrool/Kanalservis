e812a819ec52a93b04245147dc123f7b22df0273
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-06-11 23:23:17 +0500

    quantity controller was removed

--- server/controller.js
+++ server/controller.js
@@ -5,11 +5,6 @@ class Controller {
         const items = await db.query('SELECT * FROM items');
         response.json(items.rows);
     }
-
-    async getItemsQuantity(request, response) {
-        const items = await db.query('SELECT COUNT(*) FROM items');
-        response.json(Number(items.rows[0]["count"]));
-    }
 }
 
 module.exports = new Controller();
--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,6 @@ const itemsController = require('./controller');
 
 const router = new Router();
 
-router.get('/items', itemsController.getItems);
-router.get('/quantity', itemsController.getItemsQuantity);
+router.get('/', itemsController.getItems);
 
 module.exports = router;
